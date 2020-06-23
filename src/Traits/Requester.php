cc3fc18aab512a3d41308842bcff66f8259647b3
netwarp <resethread@gmail.com>
2020-06-23 17:13:58 +0200

    fix composer name

---
+++ src/Traits/Requester.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace DeroPHP\Traits;
+
+trait Requester {
+
+    public function request(string $method, $params = null, $verb = 'POST', $path = 'json_rpc') {
+        $url = "http://{$this->address}:{$this->port}/$path";
+
+        $data = [
+            'jsonrpc' => '2.0',
+            'id' => '1',
+            'method' => $method
+        ];
+
+        if ($params) {
+            $data['params'] = $params;
+        }
+
+        $options = [
+            'http' => [
+                'header' => 'Content-Type: application/json',
+                'method' => $verb,
+                'content'=> json_encode($data, JSON_FORCE_OBJECT)
+            ]
+        ];
+
+        $context = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+
+
+        if ($result === FALSE) {
+
+        }
+        else {
+            header('Content-Type: application/json');
+            return $result;
+        }
+    }
+}
--- src/Wallet.php
+++ src/Wallet.php
@@ -59,9 +59,47 @@ class Wallet {
     }
 
     public function transfer(int $amount, string $address, int $Mixin = 6, string $payment_id = '') {
-        
+        $params = compact('amount', 'address', 'Mixin', 'payment_id');
+
+        return $this->request('transfer', $params);
+    }
+
+    public function get_bulk_payments(string $payment_ids, int $min_block_height) {
+        $params = compact('payment_ids', 'min_block_height');
+
+        return $this->request('get_bulk_payments', $params);
+    }
+
+    public function query_key(string $key_type) {
+        $params = compact('key_type');
+
+        return $this->request('get_bulk_payments', $params);
+    }
+
+    public function make_integrated_address(string $payment_id) {
+        $params = compact('payment_id');
+
+        return $this->request('make_integrated_address', $params);
+    }
+
+    public function split_integrated_address(string $integrated_address) {
+        $params = compact('integrated_address');
+
+        return $this->request('split_integrated_address', $params);
+    }
+
+    public function get_transfer_by_txid(string $txid) {
+        $params = compact('txid');
+
+        return $this->request('get_transfer_by_txid', $params);
+    }
+
+    public function get_transfers(bool $In = true, bool $Out = true, int $Min_Height = 0, int $Max_Heigh = 0) {
+        $params = compact('In', 'Out', 'Min_Height', 'Max_Heigh');
+
+        return $this->request('get_transfers', $params);
     }
 }
 
 $wallet = new Wallet('127.0.0.1', '30309');
-echo $wallet->getheight();
+echo $wallet->get_transfers();
